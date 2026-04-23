46c3d33ca54cb5a1eef006cc4e906fc382ba3fb6
Eddie <edward@dcspark.io>
2026-04-23 00:53:21 -0400

    evm update w/contract @ mainnet

--- packages/shared/contracts/evm/hardhat.config.ts
+++ packages/shared/contracts/evm/hardhat.config.ts
@@ -31,6 +31,10 @@ const nodeTasks = createNodeTasks({
   fs,
 });
 
+const evmMainPort = 8545;
+const evmMainChainId = 31337;
+const evmMainInterval = 1000;
+
 // Create unified config with default networks
 const config: HardhatUserConfig = createHardhatConfig({
   sourcesDir: `${__dirname}/contracts`,
@@ -38,6 +42,36 @@ const config: HardhatUserConfig = createHardhatConfig({
   cacheDir: `${__dirname}/build/cache/hardhat`,
   tasks: nodeTasks,
   solidityVersion: "0.8.27",
+  networks: {
+    // This is needed to set once, to deploy contracts in mainet:
+    // deno task -f @go-fish/evm-contracts deploy:mainnet
+    // deno task -f @go-fish/evm-contracts build:mod
+
+    arbitrum: {
+      type: 'http',
+      chainId: 42161,
+      url: 'https://arb-mainnet.g.alchemy.com/v2/API-KEY',
+      accounts: ['0000000000000000000000000000000000000000000000000000000000000000'],
+    },
+
+    // These are development networks.
+    evmMain: {
+      type: "edr-simulated",
+      chainType: "l1",
+      chainId: evmMainChainId,
+      mining: {
+        auto: true,
+        interval: evmMainInterval, // Arbitrum (250ms)
+      },
+      allowBlocksWithSameTimestamp: true,
+    },
+    // This is a helper network to allow to hardhat/ignition to connect to the network.
+    evmMainHttp: {
+      type: "http",
+      chainType: "l1",
+      url: `http://0.0.0.0:${evmMainPort}`,
+    },
+  }
 });
 
 export default config;
